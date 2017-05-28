98cb5d7ada7de812ea52e5129094f1be3d66b286
Mayra <mayra@github.com>
2017-05-27 23:56:09 -0500

    Initial commit

---
+++ app/src/main/java/bernal/mayra/tab2app/FragmentoCuatro.java
@@ -0,0 +1,33 @@
+package bernal.mayra.tab2app;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentoCuatro extends Fragment {
+
+
+    public FragmentoCuatro() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View rootView = inflater.inflate(R.layout.fragment_fragmento_cuatro , container, false);
+
+        return rootView;
+    }
+
+    }
+
---
+++ app/src/main/java/bernal/mayra/tab2app/FragmentoDos.java
@@ -0,0 +1,29 @@
+package bernal.mayra.tab2app;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentoDos extends Fragment {
+
+
+    public FragmentoDos() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragmento_dos2, container, false);
+    }
+
+}
